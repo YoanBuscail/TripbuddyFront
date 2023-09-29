3625d7de4932d79c5180d2e96bf549f062052b91
enzo-le-gouallec <enzo.le-gouallec@oclock.school>
2023-09-29 12:07:13 +0200

    connexion en cours

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,10 +3,12 @@ import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 import LoginForm from '../LoginForm/LoginForm'
 import logoTripbuddy from "../../assets/TripBuddy-logo.png"
+import { useEffect } from 'react';
 
 function Navbar() {
     const [showNav, setShowNav] = useState(false);
     const [showLogin, setShowLogin] = useState(false);
+    const [userData, setUserData] = useState(null);
 
     const toggleNav = () => {
         setShowNav(!showNav);
@@ -15,40 +17,54 @@ function Navbar() {
         setShowLogin(!showLogin);
     };
 
+    const updateUserData = (data) => {
+        setUserData(data);
+    };
+
+    useEffect(() => {
+        const storedUserData = localStorage.getItem('userData');
+        if (storedUserData) {
+            setUserData(JSON.parse(storedUserData));
+        }
+    }, []);
 
     return (
         <>
             <header>
                 <nav className={`navbar dark-mode ${showNav ? 'show-nav' : ''}`} role="navigation">
-                    
                     <div className="navbar__logo"><img className='img-logo' src={logoTripbuddy} alt="logo trip buddy"/></div>
                     <ul className="navbar__links">
-                    <NavLink to="/" className= "navlink">
-                        <li className="navbar__link first">
-                            Accueil
-                        </li>
-                    </NavLink>
-                    <NavLink to="/itineraires" className= "navlink">
-                        <li className="navbar__link second">
-                            itinéraires
-                        </li>
-                    </NavLink>
-                    <NavLink to="/prepare-ton-voyage" className= "navlink">
-                        <li className="navbar__link third">
-                            Bien préparer son voyage
-                        </li>
-                    </NavLink>
+                        <NavLink to="/" className= "navlink">
+                            <li className="navbar__link first">
+                                Accueil
+                            </li>
+                        </NavLink>
+                        <NavLink to="/itineraires" className= "navlink">
+                            <li className="navbar__link second">
+                                itinéraires
+                            </li>
+                        </NavLink>
+                        <NavLink to="/prepare-ton-voyage" className= "navlink">
+                            <li className="navbar__link third">
+                                Bien préparer son voyage
+                            </li>
+                        </NavLink>
                     </ul>
-                    <button className='login-btn' onClick={toggleLogin}>connexion</button>
-                
+                    
+                    { userData 
+                      ? <span className='welcome-message'>Bonjour {userData.firstname}</span>
+                      : <button className='login-btn' onClick={toggleLogin}>connexion</button>
+                    }
+                    
                     <button className="burger" onClick={toggleNav}>
                         <span className="bar"></span>
                     </button>
                 </nav>
-                <LoginForm show={showLogin} toggleLogin={toggleLogin} />
+                <LoginForm show={showLogin} toggleLogin={toggleLogin} onUserLogin={updateUserData} />
             </header>
         </>
     );
 }
 
 export default Navbar;
+
