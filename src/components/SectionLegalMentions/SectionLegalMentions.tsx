abed8c214ee1bbca269080ffd7a47cc7140d90e8
enzo-le-gouallec <enzo.le-gouallec@oclock.school>
2023-10-13 16:27:42 +0200

    bug caro résolu (tkt ça arrive a tout le monde mon reuf ;)

--- src/components/SectionLegalMentions/SectionLegalMentions.tsx
+++ src/components/SectionLegalMentions/SectionLegalMentions.tsx
@@ -1,33 +1,39 @@
+import Footer from "../footer/Footer";
+import Navbar from "../navbar/Navbar";
 import "./SectionLegalMentions.css"
 
 function SectionLegalMentions() {
     return (
-        <div>
-            <h2>Mentions Légales</h2>
-            <p>
-                <h3>1. Informations légales</h3><br />
-                Nom du site web : TripBuddy<br />
-                Adresse : 38 rue oclock<br />
-                Téléphone : 02.99.23.45.45<br />
-                Adresse électronique : trippbuddy@oclock.io<br />
-                SIRET : 12357845212<br />
-                Responsable de la publication : Enzo<br />
-                Hébergeur : Merci Adélie<br />
-                <br />
-                <h3>2. Propriété intellectuelle</h3><br />
-                Le contenu de ce site, incluant, de façon non limitative, les images, graphiques, textes, vidéos, logos, et icônes sont la propriété exclusive de TripBuddy sauf mention contraire. Toute reproduction, distribution, modification, retransmission ou publication de ces éléments est strictement interdite sans l'accord écrit exprès de TripBuddy.<br />
-                <br />
-                <h3>3. Responsabilité</h3><br />
-                TripBuddy s'efforce d'assurer l'exactitude et la mise à jour des informations diffusées sur ce site, dont elle se réserve le droit de corriger, à tout moment et sans préavis, le contenu. Toutefois, TripBuddy ne peut garantir l'exactitude, la précision ou l'exhaustivité des informations mises à disposition sur ce site. En conséquence, TripBuddy décline toute responsabilité pour toute imprécision, inexactitude ou omission portant sur des informations disponibles sur le site.<br />
-                <br />
-                <h3>4. Politique de confidentialité</h3><br />
-                Veuillez consulter notre politique de confidentialité pour comprendre comment nous collectons, utilisons et protégeons vos informations personnelles.<br />
-                <br />
-                <h3>5. Droit applicable</h3><br />
-                Les présentes mentions légales sont régies par le droit français. En cas de différend et à défaut d'accord amiable, le litige sera porté devant les tribunaux français conformément aux règles de compétence en vigueur.<br />
-            </p>
+        <>
+            <Navbar />
+            <div className="legal-mentions-div">
+                <h2>Mentions Légales</h2>
+                <p>
+                    <h3>1. Informations légales</h3><br />
+                    Nom du site web : TripBuddy<br />
+                    Adresse : 38 rue oclock<br />
+                    Téléphone : 02.99.23.45.45<br />
+                    Adresse électronique : trippbuddy@oclock.io<br />
+                    SIRET : 12357845212<br />
+                    Responsable de la publication : Enzo<br />
+                    Hébergeur : Merci Adélie<br />
+                    <br />
+                    <h3>2. Propriété intellectuelle</h3><br />
+                    Le contenu de ce site, incluant, de façon non limitative, les images, graphiques, textes, vidéos, logos, et icônes sont la propriété exclusive de TripBuddy sauf mention contraire. Toute reproduction, distribution, modification, retransmission ou publication de ces éléments est strictement interdite sans l'accord écrit exprès de TripBuddy.<br />
+                    <br />
+                    <h3>3. Responsabilité</h3><br />
+                    TripBuddy s'efforce d'assurer l'exactitude et la mise à jour des informations diffusées sur ce site, dont elle se réserve le droit de corriger, à tout moment et sans préavis, le contenu. Toutefois, TripBuddy ne peut garantir l'exactitude, la précision ou l'exhaustivité des informations mises à disposition sur ce site. En conséquence, TripBuddy décline toute responsabilité pour toute imprécision, inexactitude ou omission portant sur des informations disponibles sur le site.<br />
+                    <br />
+                    <h3>4. Politique de confidentialité</h3><br />
+                    Veuillez consulter notre politique de confidentialité pour comprendre comment nous collectons, utilisons et protégeons vos informations personnelles.<br />
+                    <br />
+                    <h3>5. Droit applicable</h3><br />
+                    Les présentes mentions légales sont régies par le droit français. En cas de différend et à défaut d'accord amiable, le litige sera porté devant les tribunaux français conformément aux règles de compétence en vigueur.<br />
+                </p>
        
         </div>
+        <Footer />
+        </>
     );
 }
 
